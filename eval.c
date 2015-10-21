35da050b190672fe0dd176935a78acb730ff4ece
pooja2802m <pooja2802b@gmail.com>
2015-10-21 16:23:07 +0530

    pull addition
    addition is performed using pull

---
+++ eval.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int firstno,secondno,result;
+clrscr();
+printf("Enter first number: ");
+scanf("%d",&first);
+printf("Enter second number: ");
+scanf("%d",&second);
+result=first+second;
+printf("Addition of numbers are %d",result);
+getch();
+}
